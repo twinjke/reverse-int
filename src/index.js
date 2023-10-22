eab907c7afb4d0203905c17a0f11925c5c6b7c8c
twinjke <seventeensasha7@gmail.com>
2023-10-22 12:35:23 +0300

    feat: add-reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+  let arr = Array.from(String(n))
+
+  if (arr[0] === '-') {
+    arr.shift()
+  }
   
+  result = arr.reduce( (res, el) => res = el + res, '')
+  return Number(result)
 }
